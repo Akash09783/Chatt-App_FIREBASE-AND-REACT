5740fc4ecb48dfe854f825d974aa578ff9cda557
Akash <kumawat.aakash515@gmail.com>
2024-02-10 23:01:35 +0530

    chat component created and integrated done

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   return (
     <div >
       {room ? (
-        <Chat/>
+        <Chat room={room} />
       ) : (
         <div className="room">
           <label  >Enter Room Name</label>
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useState } from "react";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { auth, db } from "../firebase-config";
+import './style/chat.css'
+const Chat = (props) => {
+  const { room } = props;
 
-const Chat = () => {
+  const [newMessage, setNewMessage] = useState("");
+  const messageRef = collection(db, "messages");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (newMessage === "") return;
+
+    await addDoc(messageRef, {
+      text: newMessage,
+      createdAt: serverTimestamp(),
+      user: auth.currentUser.displayName,
+      room,
+    });
+    setNewMessage("");
+  };
   return (
-    <div className='chat-app'>
-    Chat  
+    <div className="chat-app">
+      <form onSubmit={handleSubmit} className="new-message-from">
+        <input
+         className="new-message-input"
+          placeholder="Type Your Message Here..."
+          onChange={(e) => setNewMessage(e.target.value)}
+          value={newMessage}
+         
+        />
+        <button type="submit" className="send-button">
+          Send
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
