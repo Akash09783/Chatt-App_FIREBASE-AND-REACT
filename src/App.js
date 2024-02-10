6fee73be35d666f0adfe048ebd510e4db7d867de
Akash <kumawat.aakash515@gmail.com>
2024-02-10 22:03:37 +0530

    authentication done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-
-import './App.css';
-import Auth from './components/Auth';
+import { useState } from "react";
+import "./App.css";
+import Auth from "./components/Auth";
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
 function App() {
+  const [isAuth, SetIsAuth] = useState(cookies.get("auth-token"));
+  const [room, setRoom] = useState("");
+  if (!isAuth) {
+    return (
+      <div className="App">
+        <Auth />
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-<Auth/>
+    <div >
+      {room ? (
+        <div>Chat</div>
+      ) : (
+        <div className="room">
+          <label  >Enter Room Name</label>
+          <input/>
+          <button>Enter Chat Room</button>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -6,9 +6,14 @@ const cookies = new Cookies()
 
 const Auth = () => {
   const signInWithGoogle = async () => {
+    try{
    const result =  await signInWithPopup(auth, provider);
  cookies.set("auth-token",result.user.refreshToken)
-  };
+}catch(err){
+  console.log(err)
+
+} 
+};
 
   return (
     <div className="auth">
