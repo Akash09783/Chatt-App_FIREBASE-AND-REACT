3438a040dee9768472e6bb751244c6a61425d9b7
Akash <kumawat.aakash515@gmail.com>
2024-02-11 10:24:53 +0530

    Chat component using firebase store done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import "./App.css";
 import Auth from "./components/Auth";
 import Chat from "./components/Chat";
 import Cookies from "universal-cookie";
+import { signOut } from "firebase/auth";
+import { auth } from "./firebase-config";
 const cookies = new Cookies();
 function App() {
   const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
   const [room, setRoom] = useState(null);
 
  const roomInputRef = useRef(null)
+
+const signUserOut =async ()=>{
+await signOut(auth)
+cookies.remove("auth=token")
+setIsAuth(false)
+setRoom(null)
+
+}
+
   if (!isAuth) {
     return (
       <div className="App">
@@ -18,7 +29,7 @@ function App() {
   }
 
   return (
-    <div >
+    < >
       {room ? (
         <Chat room={room} />
       ) : (
@@ -28,7 +39,10 @@ function App() {
           <button onClick={()=>setRoom(roomInputRef.current.value)}>Enter Chat Room</button>
         </div>
       )}
-    </div>
+      <div className="sign-out">
+        <button onClick={signUserOut}>Sign Out</button>
+      </div>
+    </>
   );
 }
 
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,34 +1,73 @@
-import React, { useState } from "react";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
-import { auth, db } from "../firebase-config";
-import './style/chat.css'
-const Chat = (props) => {
-  const { room } = props;
+import React, { useState, useEffect } from "react";
+import { db, auth } from "../firebase-config";
+import {
+  collection,
+  addDoc,
+  where,
+  serverTimestamp,
+  onSnapshot,
+  query,
+  orderBy,
+} from "firebase/firestore";
 
+import './style/chat.css';
+
+const Chat = ({ room }) => {
+  const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
-  const messageRef = collection(db, "messages");
+  const messagesRef = collection(db, "messages");
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  useEffect(() => {
+    const queryMessages = query(
+      messagesRef,
+      where("room", "==", room),
+      orderBy("createdAt")
+    );
+    const unsubscribe = onSnapshot(queryMessages, (snapshot) => {
+      let messages = [];
+      snapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      console.log(messages);
+      setMessages(messages);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
     if (newMessage === "") return;
-
-    await addDoc(messageRef, {
+    await addDoc(messagesRef, {
       text: newMessage,
       createdAt: serverTimestamp(),
       user: auth.currentUser.displayName,
       room,
     });
+
     setNewMessage("");
   };
+
   return (
     <div className="chat-app">
-      <form onSubmit={handleSubmit} className="new-message-from">
+      <div className="header">
+        <h1>Welcome to: {room.toUpperCase()}</h1>
+      </div>
+      <div className="messages">
+        {messages.map((message) => (
+          <div key={message.id} className="message">
+            <span className="user">{message.user}:</span> {message.text}
+          </div>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit} className="new-message-form">
         <input
-         className="new-message-input"
-          placeholder="Type Your Message Here..."
-          onChange={(e) => setNewMessage(e.target.value)}
+          type="text"
           value={newMessage}
-         
+          onChange={(event) => setNewMessage(event.target.value)}
+          className="new-message-input"
+          placeholder="Type your message here..."
         />
         <button type="submit" className="send-button">
           Send
@@ -37,5 +76,4 @@ const Chat = (props) => {
     </div>
   );
 };
-
-export default Chat;
+export default Chat
